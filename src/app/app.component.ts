a55a249765567b70ed1a5d5470b5e2162eea94c8
Irène Archimbaud <irene.archimbaud@myre.fr>
2024-09-06 09:16:08 +0200

    review quentin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,18 @@ import {CardListComponent} from './card/cardList.component';
 import {InputOutputComponent} from "./Exemple/input-output/input-output.component";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [ProductComponent, CardListComponent, InputOutputComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [ProductComponent, CardListComponent, InputOutputComponent],
 })
 
 export class AppComponent {
-    protected texts: string[] = ['coucou', 'bonjour', 'salut'];
+  protected texts = ['coucou', 'bonjour', 'salut'];
 
-    protected _alertText(text: string) {
-        alert(text);
-    }
+  protected _alertText(text: string) {
+    alert(text);
+  }
 }
