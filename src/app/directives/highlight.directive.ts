87780569b7f8811722bd627ba216c53b7fa1c0f1
Irène Archimbaud <irene.archimbaud@myre.fr>
2024-09-19 16:09:21 +0200

    review quentin

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,13 +1,17 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
+import {Directive, ElementRef, input, Renderer2} from "@angular/core";
 
 @Directive({
   standalone: true,
   selector: "[highlight]",
+  host: {
+    '(mouseenter)': '_onMouseEnter()',
+    '(mouseleave)': '_onMouseLeave()',
+  }
 })
 
 export class HighlightDirective {
 
-  @Input() color = 'pink';
+  public color = input<string>("pink");
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
   }
@@ -16,11 +20,12 @@ export class HighlightDirective {
     this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
   }
 
-  @HostListener('mouseenter') onMouseEnter() {
-    this._setBackGroundColor(this.color);
+  protected _onMouseEnter() {
+    this._setBackGroundColor(this.color());
   }
 
-  @HostListener('mouseleave') onMouseLeave() {
+  protected _onMouseLeave() {
     this._setBackGroundColor('transparent');
   }
+
 }
